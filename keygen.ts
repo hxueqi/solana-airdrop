82a2c3b39c22cbea83f776e30cddda24003eb96d
XueqiHuang <hxueqitao@gmail.com>
2024-06-28 22:45:54 +0200

    FIRST TRANSFER

--- keygen.ts
+++ keygen.ts
@@ -2,8 +2,6 @@ import { Keypair } from "@solana/web3.js";
 
 //Generate a new keypair
 let kp = Keypair.generate()
-if(kp){
-    console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`)
-    console.log(`Solana Wallet Secret Key: ${kp.secretKey}]`);
-}
+console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`)
+console.log(`Solana Wallet Secret Key: ${kp.secretKey}]`);
 
